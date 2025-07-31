57b4c3bce8f6500e3ec8778480cdf769f25a8a6a
Kjartan <kjartan@oskarsson.nl>
2025-07-31 11:22:13 +0200

    Login route

---
+++ src/app/login/route.js
@@ -0,0 +1,20 @@
+import { connectToMongo } from "../../lib/mongo"
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+    const body = await req.json();
+    const { subject } = body;
+
+    if (!subject) {
+        return NextResponse.json({ certificate: null });
+    }
+    
+    await connectToMongo();
+    const dbCertificate = await usersCollection.findOne({ _id: subject });
+
+    if (!dbCertificate) {
+        return NextResponse.json({ certificate: null });
+    }
+
+    return NextResponse.json({ certificate: dbCertificate.signedCertificate });
+}
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,20 +1,29 @@
 "use client"
 
 import { useContext, createContext, useState, useEffect } from "react";
+import { useWalletContext } from "../context/walletContext";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
     const [certificate, setCertificate] = useState(null);
+    const { userWallet } = useWalletContext();
 
     useEffect(() => {
         async function loginWithCertificate() {
           // Check db for user certificate
-          const response = await fetch('/api/login', {
+          if (!userWallet) {
+            return;
+          }
+          
+          const { publicKey } = userWallet.getPublicKey({ identityKey: true });
+
+          const response = await fetch('/login', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
             },
+            body: JSON.stringify({ subject: publicKey }),
           });
     
           const data = await response.json();
