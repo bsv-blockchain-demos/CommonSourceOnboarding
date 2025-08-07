2a91755530cdc45c2fdbd1824e42e257f704f172
Kjartan <kjartan@oskarsson.nl>
2025-08-07 11:46:10 +0200

    Verification setup for later

--- src/app/page.js
+++ src/app/page.js
@@ -35,6 +35,20 @@ export default function Home() {
       }
       const wallet = userWallet;
 
+      // // In production check the db if the user is verified before proceeding
+      // const res = await fetch('/emailVerify', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify({ email, type: 'check-verified' }),
+      // });
+      // const jsonRes = await res.json();
+      // if (!jsonRes.verificationStatus) {
+      //   toast.error(jsonRes.message);
+      //   return;
+      // }
+
       // Step 1: Create user DID first
       console.log('Creating user DID...');
       const didResult = await createUserDid();
@@ -86,7 +100,7 @@ export default function Home() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email, type: 'delete-on-verified' }),
+      body: JSON.stringify({ email, type: 'delete-on-verified' }), // For production change to 'verified'
     });
     if (!res.ok) {
       toast.error("Something failed, please try again");
